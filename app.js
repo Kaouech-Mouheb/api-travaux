91f62845e9d4513858d4890d3e8b1bd83331ffdd
mouheb <mouheb@MacBook-Pro-de-mouheb.local>
2024-01-14 22:40:33 +0100

    origin main

--- app.js
+++ app.js
@@ -25,18 +25,21 @@ var app = express();
 
 const cors = require('cors');
 
+app.use(cors({
+  origin: 'https://app-travaux-4ddc429d1e0f.herokuapp.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Credentials", true);
+  res.setHeader('Access-Control-Allow-Origin', 'https://app-travaux-4ddc429d1e0f.herokuapp.com');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
