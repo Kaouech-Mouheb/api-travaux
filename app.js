bdcfc0b34802187b012c023cc041993b854bdbec
mouheb <mouheb@MacBook-Pro-de-mouheb.local>
2024-01-14 22:27:26 +0100

    mise

--- app.js
+++ app.js
@@ -33,12 +33,7 @@ app.use(cors({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://app-travaux-4ddc429d1e0f.herokuapp.com');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+
 
 app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
